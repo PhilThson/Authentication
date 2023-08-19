69dad4fc18f9332782450f7fe86beb97bdd3aafa
PhilThson <phil.nevermind@gmail.com>
2023-08-19 22:00:39 +0200

    Create User endpoints

--- Authentication.Api/Program.cs
+++ Authentication.Api/Program.cs
@@ -47,64 +47,10 @@ app.UseAuthentication();
 
 app.UseAuthorization();
 
+app.MapUserEndpoints();
+
 app.MapGet("/weatherforecast", () => { })
 .WithName("GetWeatherForecast")
 .WithOpenApi();
-
-app.MapPost("/authenticate", async (AuthenticateRequestDto model) =>
-{
-    using var scope = app.Services.CreateScope();
-    var userService = scope.ServiceProvider.GetRequiredService<IUserService>();
-    var result = await userService.Authenticate(model);
-    return Results.Ok(result);
-})
-.WithName("Authenticate")
-.WithOpenApi()
-.AllowAnonymous();
-
-app.MapPost("/refreshToken", async (string refreshToken) =>
-{
-    using var scope = app.Services.CreateScope();
-    var userService = scope.ServiceProvider.GetRequiredService<IUserService>();
-    var result = await userService.RefreshToken(refreshToken);
-    return Results.Ok(result);
-})
-.WithName("RefreshToken")
-.WithOpenApi()
-.AllowAnonymous();
-
-app.MapGet("/whoAmI", async ctx =>
-{
-    ctx.Response.StatusCode = 200;
-    await ctx.Response
-        .WriteAsync(ctx.User?.Claims
-            .FirstOrDefault(x => x.Type == AuthConstants.UserIdClaim)
-            ?.Value);
-})
-.WithName("User Info")
-.WithOpenApi()
-.RequireAuthorization(AuthConstants.TokenPolicy);
-
-app.MapGet("/user/{id}", async (int id) =>
-{
-    using var scope = app.Services.CreateScope();
-    var userService = scope.ServiceProvider.GetRequiredService<IUserService>();
-    var readUserDto = await userService.GetById(id);
-    return readUserDto;
-})
-.WithName("Get user by Id")
-.WithOpenApi()
-.RequireAuthorization(AuthConstants.TokenPolicy);
-
-app.MapPost("/register", async (CreateUserDto createUserDto) =>
-{
-    using var scope = app.Services.CreateScope();
-    var userService = scope.ServiceProvider.GetRequiredService<IUserService>();
-    var readUserDto = await userService.Register(createUserDto);
-    return readUserDto;
-})
-.WithName("Register user")
-.WithOpenApi()
-.AllowAnonymous();
 
 app.Run();
